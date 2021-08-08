f2bfaa2ea37fcf0cf51785aa2a3226d011f5a9cc
Tim Godin <support@kitebot.ca>
2021-08-08 07:01:59 -0600

    switching computers

---
+++ src/lib/components/TinyTile.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let tileText = '타일'
+</script>
+
+<div class="flex">
+    <div class="flex-auto">
+        <p>{tileText}</p>
+    </div>
+</div>
+
+<style>
+
+
+    p {
+        height: 15px;
+        width: 25px;
+        font-size: small;
+        border: pink 2px solid;
+        background-color: black;
+        /* font-weight: bolder; */
+        color: white;
+        text-align: center;
+        border-top-right-radius: 0.8em;
+        border-top-left-radius: 0.8em;
+        padding: 6px;
+    }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,6 @@
 <script lang="typescript">
     import '$lib/styles.css';
+    import TinyTile from '$lib/components/TinyTile.svelte'
     import {preferences} from '$lib/localStore'
 </script>
 
@@ -9,16 +10,20 @@
 
 <h1>PlayFab + Svelte</h1>
 <p> 
-    <a target="_blank" href="https://kitebot.ca">Kitebot</a> - 
-    <a target="_blank" href="https://kit.svelte.dev/docs">SvelteKit</a> - 
-    <a href="/">Home</a> - 
-    <a target="_blank" href="https://docs.microsoft.com/en-us/rest/api/playfab/client/?view=playfab-rest">PlayFab</a> - 
-    <a target="_blank" href="https://github.com/6vx/playFabSvelte">Github</a>
+    <a target="_blank" href="https://kitebot.ca">Kitebot</a>|<a target="_blank" href="https://github.com/6vx/playFabSvelte">Github</a> - 
+    <a target="_blank" href="https://svelte.dev/docs">Svelte</a>|<a target="_blank" href="https://kit.svelte.dev/docs">Kit</a> - 
+    <a target="_blank" href="https://developer.playfab.com/en-US/my-games">Play</a>|<a target="_blank" href="https://docs.microsoft.com/en-us/rest/api/playfab/client/?view=playfab-rest">Fab</a>
+</p>
+<p>
+    <a href="/">Home</a>
 </p>
 
 <slot>
 
 </slot>
+<!-- <div class="tiles">
+    <TinyTile />
+</div> -->
 
 <style>
     p {
