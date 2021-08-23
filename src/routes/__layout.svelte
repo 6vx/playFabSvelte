3bd641155067b86ab1f69b3c8a3d749973f64a13
Tim Godin <support@kitebot.ca>
2021-08-22 21:00:16 -0600

    added meta tags for search visibility

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,6 +7,8 @@
 
 <svelte:head>
     <title>PlayFab + Svelte</title>
+    <meta name="description" content="Demo App demonstrating PlayFab and Svelte components individually. Some combinations implemented in deathMetta.">
+    <meta name="keywords" content="word, game, rpg, canadian, software, developer, kitebot, playfab + svelte, deathmetta, xbox, microsoft, word game, svelte, playfab">
 </svelte:head>
 
 <Menu />
